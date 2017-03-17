5c48bb33ef669547ca300629a91f2224b611fa22
Per Nyberg <per_skuru@msn.com>
2017-03-17 13:52:40 +0100

    Fixed som fixed fixes

--- server.js
+++ server.js
@@ -3,27 +3,47 @@ var express = require('express')
   , app = express()
   , template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
   , test = require('jade').compileFile(__dirname + '/source/templates/other.jade')
-  , albinso = "http://www.albinso.com"
+  , drinking = require('jade').compileFile(__dirname + '/source/templates/drinking.jade')
+
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var path = require('path');
+
+var albinso = "http://www.albinso.com"
+
+var url = 'mongodb://127.0.0.1:3000/test';
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+});
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
 
 app.get('/', function (req, res, next) {
-  try {
-    var html = template({ title: 'Home' })
-    res.send(html)
-  } catch (e) {
-    next(e)
-  }
+ 	try {
+		var html = template({ title: 'Home'})
+		res.send(html)
+	} catch (e) {
+		next(e)
+	}
 })
 
 app.get('/other', function (req, res, next) {
-  try {
-    var html = test({ title: '[Censored]' })
-    res.send(html)
-  } catch (e) {
-    next(e)
-  }
+	try {
+		var html = test({ title: '[Censored]'})
+		res.send(html)
+	} catch (e) {
+		next(e)
+	}
+})
+
+app.get('/drinking_games', function (req, res, next) {
+	try {
+		res.sendFile(path.join(__dirname+'/source/html-files/drinking_games.html'));
+	} catch (e) {
+		next(e)
+	}
 })
 
 app.get('/albinso', function (req, res, next) {
